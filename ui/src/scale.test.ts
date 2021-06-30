228b7fc5a5786db8f6c6228b249928e781f19dea
Michael Langan <michaell@allenai.org>
2021-06-30 11:12:42 -0700

    add jest and chai for tests

--- ui/src/scale.test.ts
+++ ui/src/scale.test.ts
@@ -3,6 +3,6 @@ import { computePageWidthPx } from "./scale";
 describe('computePageWidthPx', () => {
   it('computes pixel width of the PDF', () => {
     const output = computePageWidthPx(1, [0, 0, 612, 800]);
-    console.log(output);
+    expect(output).toEqual(816);
   });
 });
