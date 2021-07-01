8d332be191286416bcdce5c8ff9bde7d74df933e
Michael Langan <michaell@allenai.org>
2021-07-01 15:34:25 -0700

    useContext feels cleaner than .Consumer

--- ui/src/components/Overlay.tsx
+++ ui/src/components/Overlay.tsx
@@ -6,25 +6,22 @@ import BoundingBox from "./BoundingBox";
 type Props = {};
 
 export default function Overlay({}: Props) {
+  const { pageSize, scale } = React.useContext(PageSizeContext);
   return (
-    <PageSizeContext.Consumer>
-      {({ pageSize, scale }) => (
-        <svg
-          className="reader__page-overlay"
-          width={pageSize.width * scale}
-          height={pageSize.height * scale}
-        >
-          <BoundingBox
-            top={10}
-            left={10}
-            height={10}
-            width={10}
-            fill="#ff0000"
-            stroke="#00ff00"
-            onClick={() => window.alert('!!')}
-          />
-        </svg>
-      )}
-    </PageSizeContext.Consumer>
+    <svg
+      className="reader__page-overlay"
+      width={pageSize.width * scale}
+      height={pageSize.height * scale}
+    >
+      <BoundingBox
+        top={10}
+        left={10}
+        height={10}
+        width={10}
+        fill="#ff0000"
+        stroke="#00ff00"
+        onClick={() => window.alert("!!")}
+      />
+    </svg>
   );
 }
