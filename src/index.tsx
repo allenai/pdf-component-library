c1df089fd16e9500c9f68b81e77cb4f20165c7fe
Michael Langan <michaell@allenai.org>
2021-07-22 11:05:26 -0700

    add barrel file for exporting library things

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+// component exports
+export * from './components/BoundingBox';
+export * from './components/DocumentWrapper';
+export * from './components/HighlightOverlay';
+export * from './components/Overlay';
+export * from './components/PageSizeContext';
+export * from './components/PageWrapper';
+
+// helper exports
+export * from './rotate';
+export * from './scale';
+export * from './scroll';
