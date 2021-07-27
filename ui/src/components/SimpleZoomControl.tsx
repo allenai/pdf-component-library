59bf2394308eae22571839f41aedb036566a2a5e
Caroline Paulic <carolinepaulic@gmail.com>
2021-07-27 13:10:11 -0700

    Changed SimpleZoomControl from class to function component and updated it to use PageSizeContext instead of props

--- ui/src/components/SimpleZoomControl.tsx
+++ ui/src/components/SimpleZoomControl.tsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
 
+import { PageSizeContext } from '../library/components/PageSizeContext';
 import { PercentFormatter } from '../format';
 
-type Props = {
-  scale: number;
-  onScale: (multiplier: number) => void;
-};
+import { MouseEventHandler } from 'react-router/node_modules/@types/react';
 
-const ZOOM_MULTIPLIER = 1.2;
+export const SimpleZoomControl: React.FunctionComponent = () => {
+  const { scale, setScale } = React.useContext(PageSizeContext);
+  const ZOOM_MULTIPLIER = 1.2;
 
-export const SimpleZoomControl: React.FunctionComponent<Props> = ({ scale, onScale }: Props) => {
-  const handleScale = (multiplier: number) => {
-    return () => onScale(multiplier);
+  function handleScale(multiplier: number): MouseEventHandler {
+    return () => {
+      setScale(scale * multiplier);
+    };
   };
+
   return (
     <span>
       <a className="hacky-zoom-button" onClick={handleScale(1 / ZOOM_MULTIPLIER)}>
