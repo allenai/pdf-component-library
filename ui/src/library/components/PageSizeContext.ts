68649f43cf0cd390e8dfef4c5e62e6106fea6bc3
Caroline Paulic <carolinepaulic@gmail.com>
2021-07-27 12:56:03 -0700

    Updated PageSizeContext to include setters and getStyleFromContext function (moved from BoundingBox component)

--- ui/src/library/components/PageSizeContext.ts
+++ ui/src/library/components/PageSizeContext.ts
@@ -5,12 +5,64 @@ import { PdfPixelSize } from '../scale';
 
 export type PageSizeContextData = {
   pageSize: PdfPixelSize; // Scaled at 100%, might want a better name
-  scale: number;
   rotation: PageRotation;
+  scale: number;
+  setPageSize: Function,
+  setRotation: Function,
+  setScale: Function,
 };
 
 export const PageSizeContext = React.createContext<PageSizeContextData>({
   pageSize: { height: 0, width: 0 },
-  scale: 1,
   rotation: PageRotation.Rotate0,
+  scale: 1,
+  setPageSize: () => { },
+  setRotation: () => { },
+  setScale: () => { },
 });
+
+export type StyleSizeProps = {
+  top: number;
+  left: number;
+  height: number;
+  width: number;
+};
+
+export function getStyleFromContext(
+  top: number,
+  left: number,
+  height: number,
+  width: number,
+): StyleSizeProps {
+  const { rotation, scale, pageSize } = React.useContext(PageSizeContext);
+  switch (rotation) {
+    case PageRotation.Rotate90:
+      return {
+        top: left * scale,
+        left: (pageSize.height - height - top) * scale,
+        height: width * scale,
+        width: height * scale,
+      };
+    case PageRotation.Rotate180:
+      return {
+        top: (pageSize.height - height - top) * scale,
+        left: (pageSize.width - width - left) * scale,
+        height: height * scale,
+        width: width * scale,
+      };
+    case PageRotation.Rotate270:
+      return {
+        top: (pageSize.width - width - left) * scale,
+        left: top * scale,
+        height: width * scale,
+        width: height * scale,
+      };
+    default:
+      return {
+        top: top * scale,
+        left: left * scale,
+        height: height * scale,
+        width: width * scale,
+      };
+  }
+}
