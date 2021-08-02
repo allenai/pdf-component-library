63876262f4ed79a7d80dba18d2e7998e607a4a8e
Caroline Paulic <carolinepaulic@gmail.com>
2021-08-02 13:13:45 -0700

    Updated zoom click handlers to use callbacks triggered by updates to scale

--- ui/src/components/SimpleZoomControl.tsx
+++ ui/src/components/SimpleZoomControl.tsx
@@ -1,26 +1,37 @@
 import * as React from 'react';
-import { MouseEventHandler } from 'react-router/node_modules/@types/react';
 
 import { PercentFormatter } from '../format';
 import { PageSizeContext } from '../library/components/PageSizeContext';
 
-const { scale, setScale } = React.useContext(PageSizeContext);
-const ZOOM_MULTIPLIER = 1.2;
+
 
 export const SimpleZoomControl: React.FunctionComponent = () => {
-  function handleScale(multiplier: number): MouseEventHandler {
-    return () => {
-      setScale(scale * multiplier);
-    };
-  }
+  const { scale, setScale } = React.useContext(PageSizeContext);
+  const ZOOM_MULTIPLIER = 1.2;
+
+  const handleZoomIn = React.useCallback(
+    event => {
+      event.preventDefault();
+      setScale(scale * ZOOM_MULTIPLIER);
+    },
+    [scale]
+  );
+
+  const handleZoomOut = React.useCallback(
+    event => {
+      event.preventDefault();
+      setScale(scale * (1 / ZOOM_MULTIPLIER));
+    },
+    [scale]
+  );
 
   return (
     <span>
-      <a className="hacky-zoom-button" onClick={handleScale(1 / ZOOM_MULTIPLIER)}>
+      <a className="hacky-zoom-button" onClick={handleZoomOut}>
         -
       </a>
       <span>{PercentFormatter.format(scale)}</span>
-      <a className="hacky-zoom-button" onClick={handleScale(ZOOM_MULTIPLIER)}>
+      <a className="hacky-zoom-button" onClick={handleZoomIn}>
         +
       </a>
     </span>
