c69f9b29322234171b39e1d89ff07bd4c8429b94
Caroline Paulic <carolinepaulic@gmail.com>
2021-08-18 13:00:22 -0700

    Added text highlight example link to Header

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -5,6 +5,7 @@ import { SimpleZoomControl } from './SimpleZoomControl';
 type Props = {
   scale: number;
   handleToggleHighlightOverlay: () => void;
+  handleToggleHighlightText: () => void;
   handleZoom: (multiplier: number) => void;
   handleOpenDrawer: () => void;
   handleRotateCW: () => void;
@@ -15,8 +16,6 @@ export class Header extends React.PureComponent<Props> {
   render(): React.ReactNode {
     return (
       <div>
-        I&apos;m a header!
-        <br />
         <SimpleZoomControl scale={this.props.scale} onScale={this.props.handleZoom} />
         <br />
         <a onClick={this.props.handleOpenDrawer}>Outline</a>
@@ -24,6 +23,8 @@ export class Header extends React.PureComponent<Props> {
         <a onClick={this.props.handleRotateCCW}>↶</a>
         <br />
         <a onClick={this.props.handleToggleHighlightOverlay}>Highlight Overlay</a>
+        <br />
+        <a onClick={this.props.handleToggleHighlightText}>Highlight Text</a>
       </div>
     );
   }
