85d6b895dc76e677bbbee44c45a3272fe6ed15a8
Caroline Paulic <carolinepaulic@gmail.com>
2021-08-31 18:59:06 -0700

    Added type definition for Symbol

--- ui/src/data/types.ts
+++ ui/src/data/types.ts
@@ -21,3 +21,38 @@ export type Entity = {
     boundingBoxes: Array<BoundingBox>;
   };
 };
+
+export type Reference = {
+  id: string;
+};
+
+export type Symbol = Entity & {
+  type: ENTITY_TYPE.SYMBOL;
+  attributes: {
+    tex: string;
+    type: string;
+    mathml: string;
+    matchmlNearMatches: Array<string>;
+    isDefinition: boolean;
+    disambiguatedId: string;
+    nicknames: Array<string>;
+    diagramLabel: string | null;
+    definitions: Array<string>;
+    definingFormulas: Array<string>;
+    passages: Array<string>;
+    snippets: Array<string>;
+  };
+  relationships: {
+    equation: Reference;
+    children: Array<Reference>;
+    sentence: Reference;
+    parent: {
+      id: string | null;
+      type: string;
+    };
+    nicknameSentences: Array<Reference>;
+    definitionSentences: Array<Reference>;
+    definingFormulaEquations: Array<Reference>;
+    snippetSentences: Array<Reference>;
+  };
+};
