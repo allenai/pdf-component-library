0078d9b4bd0cb7eacbae0af8661069306a50245e
Caroline Paulic <carolinepaulic@gmail.com>
2021-09-07 08:58:15 -0700

    Temporary data models for Citation and Authors- this will get overwritten with work from Citation Data Model Definitions PR

--- ui/src/data/types.ts
+++ ui/src/data/types.ts
@@ -25,3 +25,27 @@ export type Entity = {
     boundingBoxes: Array<BoundingBox>;
   };
 };
+
+/*
+ * TODO: the following types will cause a merge conflict with the
+ * citation entity model once PR #29 for ticket #28339 is merged.
+ * Make sure that Author ID is not lost in the merge.
+ */
+export type Author = {
+  id: number;
+  name: string;
+  url?: string;
+};
+
+export type CitationPaper = {
+  abstract?: string;
+  authors: Array<Author>;
+  title: string;
+  url?: string;
+  year: number;
+};
+
+export type Citation = {
+  boundingBox: BoundingBox;
+  paper: CitationPaper;
+};
