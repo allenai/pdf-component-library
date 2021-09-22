1fd3de5e9804f2b17b9d0ff5cd2ae74aeaa1f6fa
Caroline Paulic <carolinepaulic@gmail.com>
2021-09-21 21:04:48 -0700

    Moved PDF load handlers out of Reader example app and into DocumentWrapper library component

--- ui/src/Reader.tsx
+++ ui/src/Reader.tsx
@@ -1,6 +1,5 @@
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
-import { PDFDocumentProxy } from 'pdfjs-dist/types/display/api';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { BrowserRouter, Route } from 'react-router-dom';
@@ -16,7 +15,6 @@ import { PageWrapper } from './library/components/PageWrapper';
 import { DocumentContext } from './library/context/DocumentContext';
 import { TransformContext } from './library/context/TransformContext';
 import { UiContext } from './library/context/UiContext';
-import { computePageSize } from './library/scale';
 import { Size } from './library/types';
 import { ENTITY_TYPE } from './types/entity';
 
@@ -29,31 +27,10 @@ export const Reader: React.FunctionComponent<RouteComponentProps> = () => {
   // ref for the scrollable region where the pages are rendered
   const pdfScrollableRef = React.createRef<HTMLDivElement>();
 
-  const { isShowingHighlightOverlay, isShowingTextHighlight, setErrorMessage, setIsLoading } =
+  const { isShowingHighlightOverlay, isShowingTextHighlight } =
     React.useContext(UiContext);
   const { rotation, scale } = React.useContext(TransformContext);
-  const { numPages, pageSize, setNumPages, setPageSize } = React.useContext(DocumentContext);
-
-  function onPdfLoadSuccess(pdfDoc: PDFDocumentProxy): void {
-    // getPage uses 1-indexed pageNumber, not 0-indexed pageIndex
-    pdfDoc.getPage(1).then(page => {
-      setPageSize(
-        computePageSize({
-          userUnit: page.userUnit,
-          topLeft: { x: page.view[0], y: page.view[1] },
-          bottomRight: { x: page.view[2], y: page.view[3] },
-        })
-      );
-    });
-    setIsLoading(false);
-    setNumPages(pdfDoc.numPages);
-    setErrorMessage(null);
-  }
-
-  function onPdfLoadError(error: unknown): void {
-    setIsLoading(false);
-    setErrorMessage(getErrorMessage(error));
-  }
+  const { numPages, pageSize } = React.useContext(DocumentContext);
 
   // TODO: #29079 remove this once we have real data
   function getDemoBoundingBoxSizes(): Array<Size> {
@@ -180,9 +157,6 @@ export const Reader: React.FunctionComponent<RouteComponentProps> = () => {
           <DocumentWrapper
             className="reader__main"
             file={TEST_PDF_URL}
-            // TODO: #28926 Improve performance by using callbacks for load handlers
-            onLoadError={onPdfLoadError}
-            onLoadSuccess={onPdfLoadSuccess}
             inputRef={pdfContentRef}>
             <Outline parentRef={pdfContentRef} />
             <div className="reader__page-list" ref={pdfScrollableRef}>
@@ -203,20 +177,3 @@ export const Reader: React.FunctionComponent<RouteComponentProps> = () => {
     </BrowserRouter>
   );
 };
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function getErrorMessage(error: any): string {
-  if (!error) {
-    return 'Unknown error';
-  }
-  if (typeof error === 'string') {
-    return error;
-  }
-  if (error instanceof Error) {
-    return error.message;
-  }
-  if (typeof error.error === 'string') {
-    return error.error;
-  }
-  return error.toString();
-}
