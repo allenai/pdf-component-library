c6bf1c8fb83b8b9cbe6a6169bcb0ecb4f138be58
Caroline Paulic <carolinepaulic@gmail.com>
2021-09-21 22:50:39 -0700

    Updated TextHighlightDemo to match new sample paper

--- ui/src/components/TextHighlightDemo.tsx
+++ ui/src/components/TextHighlightDemo.tsx
@@ -5,60 +5,81 @@ import { UiContext } from '../library/context/UiContext';
 import { BoundingBox as BoundingBoxType } from '../library/types';
 
 type Props = {
-  // TODO: #28926 subtask choose between pageNumber/pageIndex
-  pageIndex: number;
+    // TODO: #28926 subtask choose between pageNumber/pageIndex
+    pageIndex: number;
 };
 
 /*
  * Example of BoundingBoxes used as text highlights
  */
 export const TextHighlightDemo: React.FunctionComponent<Props> = ({ pageIndex }: Props) => {
-  const { isShowingTextHighlight } = React.useContext(UiContext);
+    const { isShowingTextHighlight } = React.useContext(UiContext);
 
-  function getBoundingBoxProps(): Array<BoundingBoxType> {
-    return [
-      {
-        page: 0,
-        top: 280,
-        left: 250,
-        height: 20,
-        width: 420,
-      },
-      {
-        page: 0,
-        top: 300,
-        left: 130,
-        height: 55,
-        width: 540,
-      },
-      {
-        page: 0,
-        top: 355,
-        left: 130,
-        height: 20,
-        width: 225,
-      },
-    ];
-  }
+    function getBoundingBoxProps(): Array<BoundingBoxType> {
+        return [
+            {
+                page: 0,
+                top: 170,
+                left: 415,
+                height: 30,
+                width: 110,
+            },
+            {
+                page: 0,
+                top: 421,
+                left: 283,
+                height: 15,
+                width: 55,
+            },
+            {
+                page: 0,
+                top: 830,
+                left: 387,
+                height: 15,
+                width: 56,
+            },
+            {
+                page: 1,
+                top: 213,
+                left: 315,
+                height: 15,
+                width: 55,
+            },
+            {
+                page: 1,
+                top: 477,
+                left: 395,
+                height: 15,
+                width: 55,
+            },
+            {
+                page: 1,
+                top: 844,
+                left: 618,
+                height: 15,
+                width: 55,
+            },
+        ];
+    }
 
-  function renderHighlightedBoundingBoxes(): Array<React.ReactElement> {
-    const boxes: Array<React.ReactElement> = [];
-    getBoundingBoxProps().map((prop, i) => {
-      // Only render this BoundingBox if it belongs on the current page
-      if (prop.page === pageIndex) {
-        const props = {
-          ...prop,
-          className: 'reader__sample-text-highlight__bbox',
-          // Set isHighlighted to true for highlighted styling
-          isHighlighted: true,
-          key: i,
-        };
+    function renderHighlightedBoundingBoxes(): Array<React.ReactElement> {
+        const boxes: Array<React.ReactElement> = [];
+        getBoundingBoxProps().map((prop, i) => {
+            // Only render this BoundingBox if it belongs on the current page
+            if (prop.page === pageIndex) {
+                const props = {
+                    ...prop,
+                    className: 'reader__sample-text-highlight__bbox',
+                    // Set isHighlighted to true for highlighted styling
+                    isHighlighted: true,
+                    key: i,
+                };
 
-        boxes.push(<BoundingBox {...props} />);
-      }
-    });
-    return boxes;
-  }
+                boxes.push(<BoundingBox {...props} />);
+            }
+        });
+        return boxes;
+    }
 
-  return (isShowingTextHighlight && <div>{renderHighlightedBoundingBoxes()}</div>) || null;
+    return (isShowingTextHighlight && <div>{renderHighlightedBoundingBoxes()}</div>) || null;
 };
