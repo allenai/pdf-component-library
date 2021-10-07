ba1c30d772e45eb541e5881b44c1abfbd0ef38ff
Caroline Paulic <carolinepaulic@gmail.com>
2021-10-06 21:38:10 -0700

    Updated HighlightOverlay component to use new style util functions.

--- ui/src/library/components/HighlightOverlay.tsx
+++ ui/src/library/components/HighlightOverlay.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 
 import { DocumentContext } from '../context/DocumentContext';
 import { TransformContext } from '../context/TransformContext';
-import { BoundingBoxProps, computeStyleWithContext } from './BoundingBox';
+import { computeBoundingBoxStyle, computePageStyle } from '../styleUtils';
+import { BoundingBoxProps } from './BoundingBox';
 
 type Props = {
   children?: React.ReactElement<BoundingBoxProps> | Array<React.ReactElement<BoundingBoxProps>>;
@@ -13,41 +14,22 @@ export const HighlightOverlay: React.FunctionComponent<Props> = ({
   children,
   pageIndex,
 }: Props) => {
-  const documentContext = React.useContext(DocumentContext);
-  const transformContext = React.useContext(TransformContext);
-  const pageStyle = computeStyleWithContext(
-    0,
-    0,
-    documentContext.pageSize.height,
-    documentContext.pageSize.width,
-    documentContext,
-    transformContext
-  );
+  const { pageSize } = React.useContext(DocumentContext);
+  const { rotation, scale } = React.useContext(TransformContext);
+  const pageStyle = computePageStyle(pageSize, rotation, scale);
+  const maskId = `highlight-overlay-mask-${pageIndex}`;
 
-  const getUnmaskedArea = function (
+  const getUnmaskedArea = React.useCallback((
     boundingBoxes:
       | React.ReactElement<BoundingBoxProps>
       | Array<React.ReactElement<BoundingBoxProps>>
-  ) {
-    if (!boundingBoxes) {
-      return;
-    }
-
+  ) => {
     const boxes = Array.isArray(boundingBoxes) ? boundingBoxes : [boundingBoxes];
     return boxes.map((box, i) => {
-      const boxStyle = computeStyleWithContext(
-        box.props.top,
-        box.props.left,
-        box.props.height,
-        box.props.width,
-        documentContext,
-        transformContext
-      );
+      const boxStyle = computeBoundingBoxStyle(box.props, pageSize, rotation, scale);
       return <rect style={boxStyle} x={boxStyle.left} y={boxStyle.top} key={i} fill="black"></rect>;
     });
-  };
-
-  const maskId = `highlight-overlay-mask-${pageIndex}`;
+  }, [pageSize, rotation, scale]);
 
   return (
     <div className="reader__page-highlight-overlay" style={pageStyle}>
