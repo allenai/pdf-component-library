2d3bcc1bb05cd0a8376dcc4738da6f26562efe60
Caroline Paulic <carolinepaulic@gmail.com>
2021-10-18 22:40:22 -0700

    Added new IPDFPageProxy interface. Removed unused PdfPageSizeData and Point types.

--- ui/src/library/scale.ts
+++ ui/src/library/scale.ts
@@ -1,12 +1,9 @@
-import { PDFPageProxy } from 'pdfjs-dist/types/display/api';
-import { Dimensions, Point } from './types';
+import { Dimensions } from './types';
 
-// TODO: augh this name is terrible, it's the data from react-pdf/pdfjs that
-//       we need to compute the pixel size of the PDF's page(s).
-export type PdfPageSizeData = {
-  userUnit: number; // the default size of units in 1/72nds of an inch
-  topLeft: Point;
-  bottomRight: Point;
+// Data from react-pdf/pdfjs that we need to compute the pixel size of the PDF's page(s).
+export interface IPDFPageProxy {
+  userUnit: number, // the default size of units in 1/72nds of an inch
+  view: Array<number>, // format: [ top left x coordinate, top left y coordinate, bottom right x, bottomRight y]
 };
 
 // We assume 96 DPI for display
@@ -21,7 +18,7 @@ const USER_UNIT_DENOMINATOR = 72;
  * @param page The PDFPageProxy to calculate size for
  * @returns Pixel size of a page at 100% scale assuming 96DPI display
  */
-export function computePageDimensions(page: PDFPageProxy): Dimensions {
+export function computePageDimensions(page: IPDFPageProxy): Dimensions {
   const topLeft = { x: page.view[0], y: page.view[1] };
   const bottomRight = { x: page.view[2], y: page.view[3] };
   const PPI = (page.userUnit / USER_UNIT_DENOMINATOR) * DPI;
