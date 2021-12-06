dc47e7e5a87f9a916ab3b120bea27497f00a5e23
Caroline Paulic <carolinepaulic@gmail.com>
2021-12-05 20:57:27 -0800

    Separated eslint configs for lib and demo, fixed linting errors in library index

--- ui/library/index.ts
+++ ui/library/index.ts
@@ -2,78 +2,90 @@
  * PDF Component Library exports
  */
 
+import './less/index.less';
+
 import { BoundingBox, Props as BoundingBoxProps } from './src/components/BoundingBox';
 import { DocumentWrapper, Props as DocumentWrapperProps } from './src/components/DocumentWrapper';
 import { DownloadButton, Props as DownloadButtonProps } from './src/components/DownloadButton';
-import { HighlightOverlay, Props as HighlightOverlayProps } from './src/components/HighlightOverlay';
+import {
+  HighlightOverlay,
+  Props as HighlightOverlayProps,
+} from './src/components/HighlightOverlay';
 import { Overlay, Props as OverlayProps } from './src/components/Overlay';
-import { PageWrapper, PageProps, Props as PageWrapperProps } from './src/components/PageWrapper';
-
+import { PageProps, PageWrapper, Props as PageWrapperProps } from './src/components/PageWrapper';
 import { ContextProvider, Props as ContextProviderProps } from './src/context/ContextProvider';
-import { IDocumentContext, DocumentContext } from './src/context/DocumentContext';
+import { DocumentContext, IDocumentContext } from './src/context/DocumentContext';
 import { ITransformContext, TransformContext } from './src/context/TransformContext';
 import { IUiContext, UiContext } from './src/context/UiContext';
-
-import { isSideways, PageRotation, rotateClockwise, rotateCounterClockwise } from './src/utils/rotate';
+import {
+  isSideways,
+  PageRotation,
+  rotateClockwise,
+  rotateCounterClockwise,
+} from './src/utils/rotate';
 import { generatePageIdFromIndex, scrollToId, scrollToPdfPageIndex } from './src/utils/scroll';
-import { computeBoundingBoxStyle, computePageStyle, getPageHeight, getPageWidth } from './src/utils/style';
-
-import './less/index.less';
+import {
+  computeBoundingBoxStyle,
+  computePageStyle,
+  getPageHeight,
+  getPageWidth,
+} from './src/utils/style';
 
 export {
-    // UI Components
-    BoundingBox,
-    DocumentWrapper,
-    DownloadButton,
-    HighlightOverlay,
-    Overlay,
-    PageWrapper,
-    // Context Providers 
-    ContextProvider,
-    DocumentContext,
-    TransformContext,
-    UiContext,
-    // Utils: rotation
-    isSideways,
-    PageRotation,
-    rotateClockwise,
-    rotateCounterClockwise,
-    // Utils: scroll
-    generatePageIdFromIndex,
-    scrollToId,
-    scrollToPdfPageIndex,
-    // Utils: style 
-    computeBoundingBoxStyle,
-    computePageStyle,
-    getPageHeight,
-    getPageWidth,
-}
+  BoundingBox,
+  BoundingBoxProps,
+  computeBoundingBoxStyle,
+  computePageStyle,
+  ContextProvider,
+  ContextProviderProps,
+  DocumentContext,
+  DocumentWrapper,
+  DocumentWrapperProps,
+  DownloadButton,
+  DownloadButtonProps,
+  generatePageIdFromIndex,
+  getPageHeight,
+  getPageWidth,
+  HighlightOverlay,
+  HighlightOverlayProps,
+  IDocumentContext,
+  isSideways,
+  ITransformContext,
+  IUiContext,
+  Overlay,
+  OverlayProps,
+  PageProps,
+  PageRotation,
+  PageWrapper,
+  PageWrapperProps,
+  rotateClockwise,
+  rotateCounterClockwise,
+  scrollToId,
+  scrollToPdfPageIndex,
+  TransformContext,
+  UiContext,
+};
 
 export default {
-    // UI Components
-    BoundingBox,
-    DocumentWrapper,
-    DownloadButton,
-    HighlightOverlay,
-    Overlay,
-    PageWrapper,
-    // Context Providers 
-    ContextProvider,
-    DocumentContext,
-    TransformContext,
-    UiContext,
-    // Utils: rotation
-    isSideways,
-    PageRotation,
-    rotateClockwise,
-    rotateCounterClockwise,
-    // Utils: scroll
-    generatePageIdFromIndex,
-    scrollToId,
-    scrollToPdfPageIndex,
-    // Utils: style 
-    computeBoundingBoxStyle,
-    computePageStyle,
-    getPageHeight,
-    getPageWidth,
-}
+  BoundingBox,
+  computeBoundingBoxStyle,
+  computePageStyle,
+  ContextProvider,
+  DocumentContext,
+  DocumentWrapper,
+  DownloadButton,
+  generatePageIdFromIndex,
+  getPageHeight,
+  getPageWidth,
+  HighlightOverlay,
+  isSideways,
+  Overlay,
+  PageRotation,
+  PageWrapper,
+  rotateClockwise,
+  rotateCounterClockwise,
+  scrollToId,
+  scrollToPdfPageIndex,
+  TransformContext,
+  UiContext,
+};
