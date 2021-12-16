1de853e473e2d095ff45b388a69d58498b04b8de
yensung <george129.chen@gmail.com>
2021-12-16 14:47:48 -0800

    Comment out Outline demo file for CI

--- ui/demo/components/Outline.tsx
+++ ui/demo/components/Outline.tsx
@@ -1,31 +1,31 @@
-import { Drawer } from 'antd';
-import { Outline as PdfOutline, UiContext } from 'pdf-components-dist';
-import * as React from 'react';
+// import { Drawer } from 'antd';
+// import { Outline as PdfOutline, UiContext } from 'pdf-components-dist';
+// import * as React from 'react';
 
-type Props = {
-  parentRef: React.RefObject<HTMLDivElement>;
-};
+// type Props = {
+//   parentRef: React.RefObject<HTMLDivElement>;
+// };
 
-export const Outline: React.FunctionComponent<Props> = ({ parentRef }: Props) => {
-  const { isShowingOutline, setIsShowingOutline } = React.useContext(UiContext);
+// export const Outline: React.FunctionComponent<Props> = ({ parentRef }: Props) => {
+//   const { isShowingOutline, setIsShowingOutline } = React.useContext(UiContext);
 
-  const handleHideOutline = React.useCallback(() => {
-    setIsShowingOutline(false);
-  }, []);
+//   const handleHideOutline = React.useCallback(() => {
+//     setIsShowingOutline(false);
+//   }, []);
 
-  return (
-    <Drawer
-      title="Outline"
-      placement="left"
-      visible={isShowingOutline}
-      mask={false}
-      onClose={handleHideOutline}
-      // Passing this ref mounts the drawer "inside" the grid content area
-      // instead of using the entire browser height.
-      //@ts-ignore there's something wonky with the types here
-      getContainer={parentRef.current}
-      className="reader__outline-drawer">
-      <PdfOutline />
-    </Drawer>
-  );
-};
+//   return (
+//     <Drawer
+//       title="Outline"
+//       placement="left"
+//       visible={isShowingOutline}
+//       mask={false}
+//       onClose={handleHideOutline}
+//       // Passing this ref mounts the drawer "inside" the grid content area
+//       // instead of using the entire browser height.
+//       //@ts-ignore there's something wonky with the types here
+//       getContainer={parentRef.current}
+//       className="reader__outline-drawer">
+//       <PdfOutline />
+//     </Drawer>
+//   );
+// };
