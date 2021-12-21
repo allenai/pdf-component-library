888cdea3fc819e914ed3f39dbd67f9c6105e16ac
yensung <george129.chen@gmail.com>
2021-12-21 14:09:37 -0800

    Fix types

--- ui/library/index.ts
+++ ui/library/index.ts
@@ -15,19 +15,19 @@ import { Outline } from './src/components/outline/Outline';
 import { OutlineItem } from './src/components/outline/OutlineItem';
 import { Overlay, Props as OverlayProps } from './src/components/Overlay';
 import { PageProps, PageWrapper, Props as PageWrapperProps } from './src/components/PageWrapper';
+import { NodeDestination, OutlineNode } from './src/components/types/Outline';
+import { PageProperties, PageReference } from './src/components/types/Page';
+import {
+  BoundingBox as BoundingBoxType,
+  Dimensions,
+  Nullable,
+  Origin,
+  Size,
+} from './src/components/types/types';
 import { ContextProvider, Props as ContextProviderProps } from './src/context/ContextProvider';
 import { DocumentContext, IDocumentContext } from './src/context/DocumentContext';
 import { ITransformContext, TransformContext } from './src/context/TransformContext';
 import { IUiContext, UiContext } from './src/context/UiContext';
-import {
-  BoundingBox as BoundingBoxType,
-  Destination,
-  Dimensions,
-  Nullable,
-  Origin,
-  OutlineNode,
-  Size,
-} from './src/types';
 import {
   isSideways,
   PageRotation,
@@ -46,7 +46,6 @@ export type {
   BoundingBoxProps,
   BoundingBoxType,
   ContextProviderProps,
-  Destination,
   Dimensions,
   DocumentWrapperProps,
   DownloadButtonProps,
@@ -54,11 +53,14 @@ export type {
   IDocumentContext,
   ITransformContext,
   IUiContext,
+  NodeDestination,
   Nullable,
   Origin,
   OutlineNode,
   OverlayProps,
+  PageProperties,
   PageProps,
+  PageReference,
   PageRotation,
   PageWrapperProps,
   Size,
