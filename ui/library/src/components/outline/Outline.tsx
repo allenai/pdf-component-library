d662e50bc0b0f853e63d8371e9d3e423c33177a8
yensung <george129.chen@gmail.com>
2022-01-14 09:12:52 -0800

    Update comments

--- ui/library/src/components/outline/Outline.tsx
+++ ui/library/src/components/outline/Outline.tsx
@@ -33,16 +33,17 @@ export const Outline: React.FunctionComponent<Props> = ({ className }: Props) =>
       /*
         destArray returned by getDestination contains 5 items:
         1. Reference to the page where dest locates at
-        2. Type of dest; currently only "XYZ" is handled
-        3. X -- distance from left measured in points
-        4. Y -- distance from bottom measured in points
+        2. Types of dest; currently only "XYZ" is handled
+        3. X -- the distance from the left edge of a page to dest, measured in points
+        4. Y -- the distance from the bottom edge of a page to dest, measured in points
         5. Scale
         
         Reference: https://github.com/mozilla/pdf.js/blob/d3e1d7090ac6f582d0c277e8768ac63bbbaa1134/web/base_viewer.js#L1152
       */
+      // The second and the fifth items are left out intentionally for not being used in scrolling function.
       const [ref, , leftPoints, bottomPoints] = destArray;
-      pdfDocProxy.getPageIndex(new Ref(ref)).then(refObj => {
-        scrollToPosition(parseInt(refObj.toString()), leftPoints, bottomPoints, rotation);
+      pdfDocProxy.getPageIndex(new Ref(ref)).then(refInfo => {
+        scrollToPosition(parseInt(refInfo.toString()), leftPoints, bottomPoints, rotation);
       });
     });
   };
