9d1b67358f0eec4a94c8fdfe4cb2b2b860c30b91
Caroline Paulic <carolinepaulic@gmail.com>
2022-01-14 15:51:55 -0800

    Linting fixes

--- ui/library/src/components/ZoomInButton.tsx
+++ ui/library/src/components/ZoomInButton.tsx
@@ -3,19 +3,19 @@ import * as React from 'react';
 import { TransformContext } from '../context/TransformContext';
 
 export type Props = {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 export const ZoomInButton: React.FunctionComponent<Props> = ({ children }: Props) => {
-    const { scale, setScale, zoomMultiplier } = React.useContext(TransformContext);
+  const { scale, setScale, zoomMultiplier } = React.useContext(TransformContext);
 
-    const handleZoomIn = React.useCallback(() => {
-        setScale(scale * zoomMultiplier);
-    }, [scale, zoomMultiplier]);
+  const handleZoomIn = React.useCallback(() => {
+    setScale(scale * zoomMultiplier);
+  }, [scale, zoomMultiplier]);
 
-    return (
-        <a className="reader__zoom-btn zoom-in" onClick={handleZoomIn}>
-            {children ? children : '+'}
-        </a>
-    );
+  return (
+    <a className="reader__zoom-btn zoom-in" onClick={handleZoomIn}>
+      {children ? children : '+'}
+    </a>
+  );
 };
--- ui/library/src/components/ZoomOutButton.tsx
+++ ui/library/src/components/ZoomOutButton.tsx
@@ -3,19 +3,19 @@ import * as React from 'react';
 import { TransformContext } from '../context/TransformContext';
 
 export type Props = {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 export const ZoomOutButton: React.FunctionComponent = ({ children }: Props) => {
-    const { scale, setScale, zoomMultiplier } = React.useContext(TransformContext);
+  const { scale, setScale, zoomMultiplier } = React.useContext(TransformContext);
 
-    const handleZoomOut = React.useCallback(() => {
-        setScale(scale / zoomMultiplier);
-    }, [scale, zoomMultiplier]);
+  const handleZoomOut = React.useCallback(() => {
+    setScale(scale / zoomMultiplier);
+  }, [scale, zoomMultiplier]);
 
-    return (
-        <a className="reader__zoom-btn zoom-out" onClick={handleZoomOut}>
-            {children ? children : '-'}
-        </a>
-    );
+  return (
+    <a className="reader__zoom-btn zoom-out" onClick={handleZoomOut}>
+      {children ? children : '-'}
+    </a>
+  );
 };
