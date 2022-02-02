8c2a85b748eaae68081801ae3452bec69201aeae
Caroline Paulic <carolinepaulic@gmail.com>
2022-02-01 16:51:15 -0800

    Lint fixes

--- ui/library/test/types/boundingBox.test.ts
+++ ui/library/test/types/boundingBox.test.ts
@@ -1,62 +1,79 @@
 import { expect } from 'chai';
 
-import { BoundingBox, Dimensions, RawBoundingBox, scaleRawBoundingBox } from '../../src/components/types/boundingBox';
+import {
+  BoundingBox,
+  Dimensions,
+  RawBoundingBox,
+  scaleRawBoundingBox,
+} from '../../src/components/types/boundingBox';
 
 describe('scaleRawBoundingBox', () => {
-    it('returns a boundingBox with scaled attributes (boundingBox props == 0, pageDimensions > 0)', () => {
-        const pageDimensions: Dimensions = { height: 100, width: 200 };
-        const rawBoundingBox: RawBoundingBox = {
-            page: 0,
-            top: 0,
-            left: 0,
-            height: 0,
-            width: 0,
-        };
+  it('returns a boundingBox with scaled attributes (boundingBox props == 0, pageDimensions > 0)', () => {
+    const pageDimensions: Dimensions = { height: 100, width: 200 };
+    const rawBoundingBox: RawBoundingBox = {
+      page: 0,
+      top: 0,
+      left: 0,
+      height: 0,
+      width: 0,
+    };
 
-        const result: BoundingBox = scaleRawBoundingBox(rawBoundingBox, pageDimensions.height, pageDimensions.width);
+    const result: BoundingBox = scaleRawBoundingBox(
+      rawBoundingBox,
+      pageDimensions.height,
+      pageDimensions.width
+    );
 
-        expect(result.page).equals(0);
-        expect(result.top).equals(0);
-        expect(result.left).equals(0);
-        expect(result.height).equals(0);
-        expect(result.width).equals(0);
-    });
+    expect(result.page).equals(0);
+    expect(result.top).equals(0);
+    expect(result.left).equals(0);
+    expect(result.height).equals(0);
+    expect(result.width).equals(0);
+  });
 
-    it('returns a boundingBox with scaled attributes (boundingBox props > 0, pageDimensions == 0)', () => {
-        const pageDimensions: Dimensions = { height: 0, width: 0 };
-        const rawBoundingBox: RawBoundingBox = {
-            page: 3,
-            top: .1,
-            left: .2,
-            height: .01,
-            width: .02,
-        };
+  it('returns a boundingBox with scaled attributes (boundingBox props > 0, pageDimensions == 0)', () => {
+    const pageDimensions: Dimensions = { height: 0, width: 0 };
+    const rawBoundingBox: RawBoundingBox = {
+      page: 3,
+      top: 0.1,
+      left: 0.2,
+      height: 0.01,
+      width: 0.02,
+    };
 
-        const result: BoundingBox = scaleRawBoundingBox(rawBoundingBox, pageDimensions.height, pageDimensions.width);
+    const result: BoundingBox = scaleRawBoundingBox(
+      rawBoundingBox,
+      pageDimensions.height,
+      pageDimensions.width
+    );
 
-        expect(result.page).equals(3);
-        expect(result.top).equals(0);
-        expect(result.left).equals(0);
-        expect(result.height).equals(0);
-        expect(result.width).equals(0);
-    });
+    expect(result.page).equals(3);
+    expect(result.top).equals(0);
+    expect(result.left).equals(0);
+    expect(result.height).equals(0);
+    expect(result.width).equals(0);
+  });
 
-    it('returns a boundingBox with scaled attributes (boundingBox props > 0, pageDimensions > 0)', () => {
-        const pageDimensions: Dimensions = { height: 100, width: 200 };
-        const rawBoundingBox: RawBoundingBox = {
-            page: 3,
-            top: .1,
-            left: .2,
-            height: .01,
-            width: .02,
-        };
+  it('returns a boundingBox with scaled attributes (boundingBox props > 0, pageDimensions > 0)', () => {
+    const pageDimensions: Dimensions = { height: 100, width: 200 };
+    const rawBoundingBox: RawBoundingBox = {
+      page: 3,
+      top: 0.1,
+      left: 0.2,
+      height: 0.01,
+      width: 0.02,
+    };
 
-        const result: BoundingBox = scaleRawBoundingBox(rawBoundingBox, pageDimensions.height, pageDimensions.width);
+    const result: BoundingBox = scaleRawBoundingBox(
+      rawBoundingBox,
+      pageDimensions.height,
+      pageDimensions.width
+    );
 
-        expect(result.page).equals(3);
-        expect(result.top).equals(10);
-        expect(result.left).equals(40);
-        expect(result.height).equals(1);
-        expect(result.width).equals(4);
-    });
+    expect(result.page).equals(3);
+    expect(result.top).equals(10);
+    expect(result.left).equals(40);
+    expect(result.height).equals(1);
+    expect(result.width).equals(4);
+  });
 });
