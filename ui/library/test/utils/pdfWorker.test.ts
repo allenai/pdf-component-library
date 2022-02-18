f1fa1ebdebac3b247d47cb68320cd0ba25bd3951
smita <smitar@allenai.org>
2022-02-17 17:03:11 -0800

    add a test pdf worker version

---
+++ ui/library/test/utils/pdfWorker.test.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import { pdfjs } from 'react-pdf';
+
+import { initPdfWorker } from '../../src/utils/pdfWorker';
+
+describe('initPdfWorker', () => {
+  it('pdf worker CDN matches the check in pingdom.com', () => {
+    initPdfWorker();
+    expect(pdfjs.GlobalWorkerOptions.workerSrc).to.equal(
+      '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.6.347/pdf.worker.min.js'
+    );
+  });
+});
