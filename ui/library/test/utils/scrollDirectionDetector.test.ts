53d35ce12719e7ca8ddab2627df0b1e8a2b0f23b
ericmarsh995 <eric@ip-10-0-0-42.us-west-2.compute.internal>
2022-07-22 17:46:02 -0700

    add tests for isAtTop and scrollThreshold

--- ui/library/test/utils/scrollDirectionDetector.test.ts
+++ ui/library/test/utils/scrollDirectionDetector.test.ts
@@ -190,4 +190,179 @@ describe('scrollDirectionDetector', () => {
 
     expect(scrollDirection, 'then continue to scroll up').to.equal(ScrollDirection.UP);
   });
+
+  it('isAtTop defaults to true if page starts at top', () => {
+    const mockScroll = {
+      scrollTop: 0,
+      addEventListener: (eventName, callback) => {
+        if (eventName === 'scroll') {
+          callback();
+        }
+      },
+      removeEventListener: () => undefined,
+      querySelectorAll: () => [],
+    };
+
+    let isAtTop: Nullable<boolean> = null;
+    function setIsAtTop(top: boolean) {
+      isAtTop = top;
+    }
+
+    const scrollDirectionDetector = new ScrollDetector(
+      mockScroll as any,
+      () => undefined,
+      setIsAtTop
+    );
+    scrollDirectionDetector._onScroll();
+
+    expect(isAtTop).to.equal(true);
+  });
+
+  it('isAtTop defaults to false if page starts in middle', () => {
+    const mockScroll = {
+      scrollTop: 50,
+      addEventListener: (eventName, callback) => {
+        if (eventName === 'scroll') {
+          callback();
+        }
+      },
+      removeEventListener: () => undefined,
+      querySelectorAll: () => [],
+    };
+
+    let isAtTop: Nullable<boolean> = null;
+    function setIsAtTop(top: boolean) {
+      isAtTop = top;
+    }
+
+    const scrollDirectionDetector = new ScrollDetector(
+      mockScroll as any,
+      () => undefined,
+      setIsAtTop
+    );
+    scrollDirectionDetector._onScroll();
+
+    expect(isAtTop).to.equal(false);
+  });
+
+  it('isAtTop is true after scrolling down then all the way up', () => {
+    const mockScroll = {
+      scrollTop: 0,
+      addEventListener: (eventName, callback) => {
+        if (eventName === 'scroll') {
+          callback();
+        }
+      },
+      removeEventListener: () => undefined,
+      querySelectorAll: () => [],
+    };
+
+    let isAtTop: Nullable<boolean> = null;
+    function setIsAtTop(top: boolean) {
+      isAtTop = top;
+    }
+
+    const scrollDirectionDetector = new ScrollDetector(
+      mockScroll as any,
+      () => undefined,
+      setIsAtTop
+    );
+
+    scrollDirectionDetector._onScroll();
+    expect(isAtTop, "isAtTop defaults to true at top of page").to.equal(true);
+
+    mockScroll.scrollTop = 100;
+    scrollDirectionDetector._onScroll();
+    expect(isAtTop, "isAtTop is false since we scrolled down").to.equal(false);
+
+    mockScroll.scrollTop = 0;
+    scrollDirectionDetector._onScroll();
+    expect(isAtTop, "isAtTop is true again since we scrolled all the way up").to.equal(true); 
+  });
+
+
+
+  // test threshold
+
+ 
+  it('scrollThresholdReachedInDirection defaults to null on start', () => {
+    const mockScroll = {
+      scrollTop: 0,
+      addEventListener: (eventName, callback) => {
+        if (eventName === 'scroll') {
+          callback();
+        }
+      },
+      removeEventListener: () => undefined,
+      querySelectorAll: () => [],
+    };
+
+    const scrollThreshold = 50;
+    let scrollThresholdReachedInDirection: Nullable<ScrollDirection> = null;
+    function setScrollThresholdReachedInDirection(dir: Nullable<ScrollDirection>) {
+      scrollThresholdReachedInDirection = dir;
+    }
+ 
+    const scrollDirectionDetector = new ScrollDetector(
+      mockScroll as any,
+      () => undefined,
+      () => undefined, 
+      setScrollThresholdReachedInDirection,
+      scrollThreshold,
+    );
+    scrollDirectionDetector._onScroll();
+
+    expect(scrollThresholdReachedInDirection).to.equal(null);
+  });
+
+  it('scrollThresholdReachedInDirection is null until reaching threshold and returns correct direction in both directions', () => {
+    const mockScroll = {
+      scrollTop: 0,
+      addEventListener: (eventName, callback) => {
+        if (eventName === 'scroll') {
+          callback();
+        }
+      },
+      removeEventListener: () => undefined,
+      querySelectorAll: () => [],
+    };
+
+    const scrollThreshold = 50;
+    let scrollThresholdReachedInDirection: Nullable<ScrollDirection> = null;
+    function setScrollThresholdReachedInDirection(dir: Nullable<ScrollDirection>) {
+      scrollThresholdReachedInDirection = dir;
+    }
+ 
+    const scrollDirectionDetector = new ScrollDetector(
+      mockScroll as any,
+      () => undefined,
+      () => undefined, 
+      setScrollThresholdReachedInDirection,
+      scrollThreshold,
+    );
+    scrollDirectionDetector._onScroll();
+    expect(scrollThresholdReachedInDirection, "scrollThresholdReachedInDirection defults to null").to.equal(null);
+
+    mockScroll.scrollTop = 1;  // have to do this with mockscroll
+    scrollDirectionDetector._onScroll();
+
+    mockScroll.scrollTop = 25;  
+    scrollDirectionDetector._onScroll();
+    expect(scrollThresholdReachedInDirection, "scrolled down but not enough to hit threshold so is still null").to.equal(null);
+
+    mockScroll.scrollTop = 51;  
+    scrollDirectionDetector._onScroll();
+    expect(scrollThresholdReachedInDirection, "scrolled down enough to hit threshold and return DOWN").to.equal(ScrollDirection.DOWN);
+
+    mockScroll.scrollTop = 60;  // make some room
+    scrollDirectionDetector._onScroll();
+
+    mockScroll.scrollTop = 59;  // have to do this with mockscroll
+    scrollDirectionDetector._onScroll();
+  
+    mockScroll.scrollTop = 9;  
+    scrollDirectionDetector._onScroll(); 
+    expect(scrollThresholdReachedInDirection, "scrolled up enough to hit threshold and return UP").to.equal(ScrollDirection.UP);
+  });
+ 
 });
