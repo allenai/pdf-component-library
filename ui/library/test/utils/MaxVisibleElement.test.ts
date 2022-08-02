658f48696f5e66569446c444224097c9cdc83090
huytr1995 <huyt@allenai.org>
2022-08-02 15:12:45 -0700

    Add unit test

---
+++ ui/library/test/utils/MaxVisibleElement.test.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+import { getMaxVisibleElement } from '../../src/utils/MaxVisibleElement';
+
+describe('MaxVisibleElement', () => {
+  const map = new Map();
+  map.set(1, 0.00123);
+  map.set(2, 0.012567);
+  map.set(3, 0.2);
+
+  it('should return the correct key of the max visible element when getMaxVisibleElement get called', () => {
+    const maxVisibleElement = getMaxVisibleElement(map);
+    expect(maxVisibleElement).to.equal(3);
+  });
+});
